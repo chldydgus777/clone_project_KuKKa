9e6bc84f012e9de1ac6b935e4ee6901bdea809da
chldydgus777 <chldydgus777@naver.com>
2021-04-02 21:00:16 +0900

    add Main

--- frontend_practice/src/shared/App.js
+++ frontend_practice/src/shared/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Header from "../components/Header";
+import styled from "styled-components"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main/>      
     </div>
   );
 }
 
+const Main = styled.div`
+  background : green;
+  width : 100%;
+  height : 800px;
+`;
+
 export default App;
+
+
