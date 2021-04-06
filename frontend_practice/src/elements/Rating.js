0f4254909341be8381c8d0c1a47ace531a8d74ee
Henry-Cho <hyungseok5926@gmail.com>
2021-04-06 21:21:09 +0900

    add header feature & revise modal

--- frontend_practice/src/elements/Rating.js
+++ frontend_practice/src/elements/Rating.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Rating from "@material-ui/lab/Rating";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    background: "white",
+    padding: "14px",
     flexDirection: "column",
     "& > * + *": {
       marginTop: theme.spacing(1),
